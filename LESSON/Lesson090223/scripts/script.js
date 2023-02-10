6d1427257683dd5b70500345485ecb58d22e68fc
Henryk22 <phlife.borisov@gmail.com>
2023-02-10 13:31:55 +0100

    Lesson100223

---
+++ LESSON/Lesson090223/scripts/script.js
@@ -0,0 +1,123 @@
+console.log ("Hello Word");
+
+// const name = prompt("Write your name"); 
+// console.log("Hello " + name + "." + "How are you?"); // concatenation
+// console.log(`Hello ${name}. How are you?`); // interpolation
+
+// A-Z a-z $ _ 0-9
+
+//1a nelzja
+// a1
+// $a. a$ 
+// _
+// car name
+// car_name // snake case
+// carName // camel case
+// car-name // kebab case, wrong
+
+//let, const
+//var - benuts nicht
+
+// string nummer
+
+// console.log(5);
+// console.log("5");
+
+// console.log(typeof(name));
+// console.log(typeof(5));
+// console.log(typeof name);
+// console.log(typeof 5);
+
+// const r = 12
+// const g = 34
+// const b = 14
+
+// console.log("rgb(" + r + ", " + g + "," + b + " );"); // concatenation
+// console.log(`rgb( ${r}, ${g}, ${b} )`); // interpolation
+
+// const value_1 =12
+// const value_2 =4
+
+// console.log(value_1 + value_2);
+// console.log(value_1 - value_2);
+// console.log(value_1 * value_2);
+// console.log(value_1 / value_2);
+// console.log(value_1 % value_2); //взятие остатка от
+// console.log(value_1 ** value_2); //возведение в степень
+
+// console.log("==========================");
+// const a = 2
+// const b = "2"
+// console.log(typeof String(a));
+// console.log(typeof Number(b));
+// console.log(typeof +b);
+// console.log( typeof ("" +a));
+
+// console.log("2" + 2);
+// console.log("2" - 2);
+// console.log(Number ("a"));
+// console.log(typeof NaN); 
+
+//console.log("b" + "a" + Number("c") + "a");
+
+//
+// const Number = prompt("Write your nummer"); 
+// console.log(`Your result is ${Number * Number}`);
+
+// const num_1 = +prompt("Write first number")
+// const num_2 = +prompt("Write first number")
+
+// console.log(`num_1 + num_2{num_1 + num_2}`);
+
+// const a = "2"
+// const b = 2
+//     console.log(a == b); // true
+//     console.log(a === b);  //false
+
+//     if (a == b) {
+//         console.log("a == b");
+//     } else {
+//         console.log("a != b");
+//             }
+//             if (a === b) {
+//                 console.log("a === b");
+//             } else {
+//                 console.log("a != b");
+//                     }
+
+// if (a > b) {
+//     console.log("a > b");
+// } else {
+//     console.log(a < b);
+// }
+// if (a < b) {
+//     console.log("a < b");
+// } else {
+//     console.log(a > b);
+// }
+// if (a >= b) {
+//     console.log("a >= b");
+// } else {
+//     console.log(a = b);
+// }
+// if (a <= b) {
+//     console.log("a <= b");
+// } else {
+//     console.log(a = b);
+// }
+// if (a != b) {
+//     console.log("a != b");
+// } else {
+//     console.log(a = b);
+// }
+// if (a !== b) {
+//     console.log("a !== b");
+// } else {
+//     console.log(a = b);
+// }
+
+
+
+
+
+
