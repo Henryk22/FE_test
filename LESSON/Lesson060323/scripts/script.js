84e9a077b8294503acfcfbb03151f6ee3269a145
Henryk22 <phlife.borisov@gmail.com>
2023-03-09 19:28:50 +0100

    HW10_060323

---
+++ LESSON/Lesson060323/scripts/script.js
@@ -0,0 +1,60 @@
+// console.log("Hello");
+
+// let number = 15;
+// number = 1
+// // if (number >= 10) {
+// //   console.log('hello');
+// // } else {
+// //   console.log('hi');
+// // }
+
+// number >= 10 ? console.log('hello') : console.log('hi');;
+
+//Циклы
+
+// for (let index = 0; index <= 20; index += 2) {
+//    console.log(index);;
+       
+// }
+
+
+// for (let index = 100; index >= 57; index--) {
+//     console.log(index);
+        
+//  }
+ 
+//const numbers = [6, 7, 1, -10, 78, 18, -5, -38, 9]
+
+//// 1. Сформировать новый массив, в который попадут все положительные числа из массива numbers
+
+// const positive_numbers = [];
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = array[index];
+//     if (numbers[i] > 0) {
+//         positive_numbers.push(numbers[i])
+        
+//     }
+// }
+// console.log(positive_numbers);
+
+// const numbers = [6, 7, 1, -10, 78, 18, -5, -38, 9];
+
+// // const mult_numbers = []
+
+// // for (let index = 0; index < numbers.length; index++) {
+// //     mult_numbers.push(numbers[index] * 2)
+    
+// // }
+
+// const newNumbers = []
+// for (let index = 0; index < numbers.length; index++) {
+//     if (numbers[index] > 0){
+        
+//     newNumbers.push(numbers[index] / 2)}
+   
+    
+// }
+
+// console.log(newNumbers);
+
+
